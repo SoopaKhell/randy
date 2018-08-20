f25c5a503c8c8da49c08754cf683433f3f7a2f68
Carter Prince <CarterAPrince@gmail.com>
2018-08-19 20:46:34 -0400

    added a few random lists!

--- randy.py
+++ randy.py
@@ -2,10 +2,6 @@ import sys
 import re
 import random
 
-#print(sys.argv)
-
-modes = []
-
 code = None
 
 if len(sys.argv) > 1:
@@ -27,17 +23,30 @@ if len(sys.argv) > 1:
             print("Error: Invalid filetype! Must be a .ry file.")
 
 
-#print(code.split("\n"))
+def randomNameF():
+    with open("data/femalenames.txt", 'r') as f:
+        data = f.read().split("\n")
+    return random.choice(data)
+
+def randomNameM():
+    with open("data/malenames.txt", 'r') as f:
+        data = f.read().split("\n")
+    return random.choice(data)
+
+def randomName():
+    with open("data/names.txt", 'r') as f:
+        data = f.read().split("\n")
+    return random.choice(data)
 
 def randomAnimal():
     with open("data/animals.txt", 'r') as f:
-        listOfAnimals = f.read().split("\n")
-    return random.choice(listOfAnimals)
+        data = f.read().split("\n")
+    return random.choice(data)
 
 def randomCountry():
     with open("data/countries.txt", 'r') as f:
-        listOfAnimals = f.read().split("\n")
-    return random.choice(listOfAnimals)
+        data = f.read().split("\n")
+    return random.choice(data)
 
 def randomDigit():
     return str(random.randint(0, 9))
@@ -45,54 +54,52 @@ def randomDigit():
 generators = {
     "animal": randomAnimal,
     "digit": randomDigit,
-    "country": randomCountry
+    "country": randomCountry,
+    "femalename": randomNameF,
+    "malename": randomNameM,
+    "name": randomName
 }
 
 
-parsedLines = []
+output = code
 
-for line in code.split('\n'):
-    offset = 0
-    randomSomething = re.finditer(r"\$\w?\[[^\[\]]*\]", line)
-    for match in randomSomething:
-        start = match.span()[0]+offset
-        stop = match.span()[1]+offset
-        randomList = line[start:stop].strip("$")
-        if randomList[0].lower() == 'u':
-            randomList = randomList[1:].strip('[]')
-            randomString = generators[randomList]().upper()
-        elif randomList[0].lower() == 't':
-            randomList = randomList[1:].strip('[]')
-            randomString = generators[randomList]().title()
-        else:
-            randomList = randomList[1:].strip('[]')
-            randomString = generators[randomList]()
-        line = line[0:start] + randomString + line[stop:]
-        offset -= ((stop-start)-len(randomString))
+offset = 0
+randomSomething = re.finditer(r"\$\w?\[[^\[\]]*\]", output)
+for match in randomSomething:
+    start = match.span()[0]+offset
+    stop = match.span()[1]+offset
+    command = output[start:stop].strip("$")
+    caseMode = command[0].lower()
+    generator = command[1:].strip('[]').lower()
+    if caseMode == 'u':
+        randomString = generators[generator]().upper()
+    elif caseMode == 't':
+        randomString = generators[generator]().title()
+    elif caseMode == 'l':
+        randomString = generators[generator]().lower()
+    else:
+        randomString = generators[generator]() #default
+    output = output[0:start] + randomString + output[stop:]
+    offset -= ((stop-start)-len(randomString))
 
-    offset = 0
-    makeChoice = re.finditer(r"\([^\(\)]*\)", line)
-    for match in makeChoice:
-        start = match.span()[0]+offset
-        stop = match.span()[1]+offset
-        options = line[start:stop].strip("()").split("|")
-        choiceString = random.choice(options)
-        line = line[0:start] + choiceString + line[stop:]
-        offset -= ((stop-start)-len(choiceString))
+offset = 0
+makeChoice = re.finditer(r"\([^\(\)]*\)", output)
+for match in makeChoice:
+    start = match.span()[0]+offset
+    stop = match.span()[1]+offset
+    options = output[start:stop].strip("()").split("|")
+    choiceString = random.choice(options)
+    output = output[0:start] + choiceString + output[stop:]
+    offset -= ((stop-start)-len(choiceString))
 
-    offset = 0
-    randomNumber = re.finditer(r"\{[^\{\}]*\}", line)
-    for match in randomNumber:
-        start = match.span()[0]+offset
-        stop = match.span()[1]+offset
-        min, max = [int(n) for n in line[start:stop].strip("{}").split("/")]
-        number = random.randint(min, max)
-        line = line[0:start] + str(number) + line[stop:]
-        offset -= ((stop-start)-len(str(number)))
+offset = 0
+randomNumber = re.finditer(r"\{[^\{\}]*\}", output)
+for match in randomNumber:
+    start = match.span()[0]+offset
+    stop = match.span()[1]+offset
+    min, max = [int(n) for n in output[start:stop].strip("{}").split("/")]
+    number = random.randint(min, max)
+    output = output[0:start] + str(number) + output[stop:]
+    offset -= ((stop-start)-len(str(number)))
 
-
-    parsedLines.append(line)
-
-
-output = '\n'.join(parsedLines)
-print(output)
+sys.stdout.write(output)
